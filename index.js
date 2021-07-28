0fd34e2f9c50914f44bd8b93754d3b80af6bea8d
taylortreece <tayloratreece@gmail.com>
2021-07-27 21:11:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+const dodger = document.getElementById("dodger")
+
+function moveDodgerLeft() {
+    let leftNumber = dodger.style.left.replace("px", "")
+    let left = parseInt(leftNumber, 10)
+    
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+function moveDodgerRight() {
+    let leftNumber = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumber, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    } else {
+        console.log("Dodger can only move left and right")
+    }
+});
